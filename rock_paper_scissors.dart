5c657f3e0c55f666da464f0134b47101605e26c6
David Castello <davidcastellobatetgmail.com>
2021-09-15 14:09:48 +0200

    implement player move

--- rock_paper_scissors.dart
+++ rock_paper_scissors.dart
@@ -1,3 +1,34 @@
+import 'dart:io';
+import 'dart:math';
+
+enum Move { rock, paper, scissors }
+
 void main() {
-  print('Hello world');
+  final rng = Random();
+  while (true) {
+    stdout.write('Rock, Paper or Scissors ? ( r / p / s / q for quit) :  ');
+    final playerInput = stdin.readLineSync();
+    if (playerInput == 'r' || playerInput == 'p' || playerInput == 's') {
+      // select player move
+      var playerMove;
+      if (playerInput == 'r') {
+        playerMove = Move.rock;
+      } else if (playerInput == 'p') {
+        playerMove = Move.paper;
+      } else {
+        playerMove = Move.scissors;
+      }
+      print('Player : $playerMove');
+      // generate random AI move
+      final random = rng.nextInt(3);
+      final aiMove = Move.values[random];
+      print('Computer : $aiMove');
+      // TODO: LOGICAL WIN / LOSE
+    } else if (playerInput == 'q') {
+      print('End Game !');
+      break;
+    } else {
+      print('Invalid selection, try again...');
+    }
+  }
 }
